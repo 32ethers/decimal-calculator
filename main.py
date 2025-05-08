26ebf2594a301fadd36815ad089aa0350b3fd7a9
32ethers <somereasons@outlook.com>
2025-05-08 17:14:47 +0800

    press ctrl + c twice to exit,

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from prompt_toolkit import PromptSession
 
 from input_decoder import process_and_calc
 
+ctrl_c_counter = 0
+
 if __name__ == '__main__':
     session = PromptSession('$ > ')
     parser = argparse.ArgumentParser(description="input parameters")
     parser.add_argument('-p', '--precision', type=int, default=30, help='The precision of Decimal')
     args = parser.parse_args()
     getcontext().prec = int(args.precision)
-    print("An high-precision command-line calculator. Please enter a mathematical expression. Press ctrl+c to exit.")
+    print("An high-precision command-line calculator. Please enter a mathematical expression. Press ctrl+c twice to exit.")
     while True:
         try:
             user_input = session.prompt()
@@ -20,6 +22,9 @@ if __name__ == '__main__':
             resu = process_and_calc(user_input)
             print(resu)
         except KeyboardInterrupt:
+            if ctrl_c_counter < 1:
+                ctrl_c_counter += 1
+                continue
             break
         except Exception as e:
             print("Error!", e)
